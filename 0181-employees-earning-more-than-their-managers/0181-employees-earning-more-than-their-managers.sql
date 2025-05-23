79adf7b54d538dea60e92b8453ff2c03b15285c6
Cometko <cometko1234@gmail.com>
2025-05-23 12:05:38 +0900

    Time: 377 ms (71.99%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 #TABLE: Employee
-#INNER JOIN
+#INNER JOIN,
+#TABLE: e1:employee's table / e2:manager's table
 
 SELECT 
- e2.name AS Employee
+ e1.name AS Employee
 FROM Employee AS e1
 INNER JOIN Employee  AS e2
-ON e1.id = e2.managerId
-WHERE e1.salary < e2.salary
-
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary
